95a3d71575d1ac50a17739828033e5a55cb8bd08
seblabrik <44549273+seblabrik@users.noreply.github.com>
2019-02-27 18:09:52 -0500

    Map deuxième version: ajout du MapController

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : MonoBehaviour
     private NavMeshSurface2d surface;
     private RoomGenerator roomGenerator;
     private LevelGenerator levelGenerator;
+    private MapController mapController;
+    private GameObject map;
 
     public Dictionary<RoomIndex, RoomParam> levelRooms;
     public RoomParam currentRoom;
@@ -23,13 +25,7 @@ public class GameManager : MonoBehaviour
     public int width = 11;//doit toujours être impaire
     public int height = 11;//doit toujours être impaire
 
-    private GameObject map;
-    private GameObject mapPlayer;
-    private float mapSizeParam = 30f;
     private List<List<RoomIndex>> connectedRooms;
-    public GameObject mapGatePrefab;
-    public GameObject mapRoomPrefab;
-    public GameObject mapPlayerPrefab;
 
     void Awake()
     {
@@ -49,6 +45,8 @@ public class GameManager : MonoBehaviour
 
         levelGenerator = GetComponent<LevelGenerator>();
 
+        mapController = GetComponent<MapController>();
+
         InitGame();
     }
 
@@ -58,7 +56,6 @@ public class GameManager : MonoBehaviour
         {
             map.SetActive(!map.activeSelf);
         }
-
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
@@ -79,7 +76,7 @@ public class GameManager : MonoBehaviour
         (levelRooms, connectedRooms) = levelGenerator.GenerateLevel();
         currentRoomIndex = new RoomIndex { abs = 0, ord = 0 };
         currentRoom = levelRooms[currentRoomIndex];
-        CreateMap();
+        map = mapController.CreateMap(levelRooms, connectedRooms);
         map.SetActive(false);
         roomGenerator.SetupRoom(currentRoom, playerSpawn);
         surface.BuildNavMesh();
@@ -89,7 +86,7 @@ public class GameManager : MonoBehaviour
     {
         surface = Instantiate(surfacePrefab);
         currentRoom = GetNextRoom();
-        CreateMap();
+        mapController.UpdateMap(currentRoom);
         map.SetActive(false);
         roomGenerator.SetupRoom(currentRoom, playerSpawn);
         surface.BuildNavMesh();
@@ -107,38 +104,5 @@ public class GameManager : MonoBehaviour
         if (playerSpawn == "East") { currentRoomIndex.abs -= 1; }
         if (playerSpawn == "West") { currentRoomIndex.abs += 1; }
         return levelRooms[currentRoomIndex];
-    }
-
-    private void CreateMap()
-    {
-        map = GameObject.Find("Map");
-
-        foreach (RoomParam room in levelRooms.Values)
-        {
-            float x = room.coordonates.abs * mapSizeParam + (width - 1) / 2;
-            float y = room.coordonates.ord * mapSizeParam + (height - 1) / 2;
-            GameObject roomMap = Instantiate(mapRoomPrefab, new Vector3(x, y, 0f), Quaternion.identity);
-            roomMap.transform.SetParent(map.transform, false);
-            
-        }
-        List<string> closedList = new List<string>();
-
-        foreach (List<RoomIndex> connection in connectedRooms)
-        {
-            string id1 = connection[0].abs + "" + connection[0].ord;
-            string id2 = connection[1].abs + "" + connection[1].ord;
-            if (closedList.Contains(id1 + id2) || closedList.Contains(id2 + id1)) { continue; }
-            closedList.Add(id1 + id2);
-            closedList.Add(id2 + id1);
-            float x = ((connection[0].abs + connection[1].abs) * mapSizeParam + (width - 1)) / 2;
-            float y = ((connection[0].ord + connection[1].ord) * mapSizeParam + (height - 1)) / 2;
-            GameObject gate = Instantiate(mapGatePrefab, new Vector3(x, y, 0f), Quaternion.identity);
-            gate.transform.SetParent(map.transform, false);
-        }
-        float x_ = currentRoom.coordonates.abs * mapSizeParam + (width - 1) / 2;
-        float y_ = currentRoom.coordonates.ord * mapSizeParam + (height - 1) / 2;
-        mapPlayer = Instantiate(mapPlayerPrefab, new Vector3(x_, y_, 0f), Quaternion.identity);
-        mapPlayer.transform.SetParent(map.transform, false);
-        mapPlayer.transform.SetAsLastSibling();
     }
 }
---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,73 @@
+﻿
+using LJSM.Models;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapController : MonoBehaviour
+{
+
+    public float mapSizeParam = 30f;
+
+    public GameObject mapPrefab;
+    public GameObject mapGatePrefab;
+    public GameObject mapRoomPrefab;
+    public GameObject mapPlayerPrefab;
+    public GameObject canvas;
+
+    private GameObject map;
+    private GameObject mapPlayer;
+
+    private int width;
+    private int height;
+
+
+    public GameObject CreateMap(Dictionary<RoomIndex, RoomParam> levelRooms, List<List<RoomIndex>> connectedRooms)
+    {
+        canvas = GameObject.Find("Canvas");
+        map = Instantiate(mapPrefab, canvas.transform);
+
+        DontDestroyOnLoad(canvas);
+
+        width = GameManager.instance.width;
+        height = GameManager.instance.height;
+
+        foreach (RoomParam room in levelRooms.Values)
+        {
+            float x = room.coordonates.abs * mapSizeParam + (width - 1) / 2;
+            float y = room.coordonates.ord * mapSizeParam + (height - 1) / 2;
+            GameObject roomMap = Instantiate(mapRoomPrefab, new Vector3(x, y, 0f), Quaternion.identity);
+            roomMap.transform.SetParent(map.transform, false);
+        }
+        List<string> closedList = new List<string>();
+
+        foreach (List<RoomIndex> connection in connectedRooms)
+        {
+            string id1 = connection[0].abs + "" + connection[0].ord;
+            string id2 = connection[1].abs + "" + connection[1].ord;
+            if (closedList.Contains(id1 + id2) || closedList.Contains(id2 + id1)) { continue; }
+            closedList.Add(id1 + id2);
+            closedList.Add(id2 + id1);
+            float x = ((connection[0].abs + connection[1].abs) * mapSizeParam + (width - 1)) / 2;
+            float y = ((connection[0].ord + connection[1].ord) * mapSizeParam + (height - 1)) / 2;
+            GameObject gate = Instantiate(mapGatePrefab, new Vector3(x, y, 0f), Quaternion.identity);
+            gate.transform.SetParent(map.transform, false);
+        }
+        mapPlayer = Instantiate(mapPlayerPrefab, new Vector3((width - 1) / 2, (height - 1) / 2, 0f), Quaternion.identity);
+        mapPlayer.transform.SetParent(map.transform, false);
+        mapPlayer.transform.SetAsLastSibling();
+
+        return map;
+    }
+
+    public void UpdateMap(RoomParam currentRoom)
+    {
+        Destroy(mapPlayer);
+        float x  = currentRoom.coordonates.abs * mapSizeParam + (width - 1) / 2;
+        float y = currentRoom.coordonates.ord * mapSizeParam + (height - 1) / 2;
+        mapPlayer = Instantiate(mapPlayerPrefab, new Vector3(x, y, 0f), Quaternion.identity);
+        mapPlayer.transform.SetParent(map.transform, false);
+        mapPlayer.transform.SetAsLastSibling();
+    }
+}
+
