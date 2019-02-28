9c0b4a69727e0e0c7b112e8fb44f47ca7d5a1d41
seblabrik <44549273+seblabrik@users.noreply.github.com>
2019-02-27 19:45:53 -0500

    Map troisième version: ajout du drag

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -10,13 +10,16 @@ public class MapController : MonoBehaviour
     public float mapSizeParam = 30f;
 
     public GameObject mapPrefab;
+    public GameObject mapBackgroundPrefab;
     public GameObject mapGatePrefab;
     public GameObject mapRoomPrefab;
     public GameObject mapPlayerPrefab;
-    public GameObject canvas;
+    public GameObject mapDragablePrefab;
 
-    private GameObject map;
+    private GameObject canvas;
+    private GameObject dragableMap;
     private GameObject mapPlayer;
+    private GameObject map;
 
     private int width;
     private int height;
@@ -26,6 +29,8 @@ public class MapController : MonoBehaviour
     {
         canvas = GameObject.Find("Canvas");
         map = Instantiate(mapPrefab, canvas.transform);
+        Instantiate(mapBackgroundPrefab, map.transform);
+        dragableMap = Instantiate(mapDragablePrefab, map.transform);
 
         DontDestroyOnLoad(canvas);
 
@@ -37,7 +42,7 @@ public class MapController : MonoBehaviour
             float x = room.coordonates.abs * mapSizeParam + (width - 1) / 2;
             float y = room.coordonates.ord * mapSizeParam + (height - 1) / 2;
             GameObject roomMap = Instantiate(mapRoomPrefab, new Vector3(x, y, 0f), Quaternion.identity);
-            roomMap.transform.SetParent(map.transform, false);
+            roomMap.transform.SetParent(dragableMap.transform, false);
         }
         List<string> closedList = new List<string>();
 
@@ -51,10 +56,10 @@ public class MapController : MonoBehaviour
             float x = ((connection[0].abs + connection[1].abs) * mapSizeParam + (width - 1)) / 2;
             float y = ((connection[0].ord + connection[1].ord) * mapSizeParam + (height - 1)) / 2;
             GameObject gate = Instantiate(mapGatePrefab, new Vector3(x, y, 0f), Quaternion.identity);
-            gate.transform.SetParent(map.transform, false);
+            gate.transform.SetParent(dragableMap.transform, false);
         }
         mapPlayer = Instantiate(mapPlayerPrefab, new Vector3((width - 1) / 2, (height - 1) / 2, 0f), Quaternion.identity);
-        mapPlayer.transform.SetParent(map.transform, false);
+        mapPlayer.transform.SetParent(dragableMap.transform, false);
         mapPlayer.transform.SetAsLastSibling();
 
         return map;
@@ -66,7 +71,7 @@ public class MapController : MonoBehaviour
         float x  = currentRoom.coordonates.abs * mapSizeParam + (width - 1) / 2;
         float y = currentRoom.coordonates.ord * mapSizeParam + (height - 1) / 2;
         mapPlayer = Instantiate(mapPlayerPrefab, new Vector3(x, y, 0f), Quaternion.identity);
-        mapPlayer.transform.SetParent(map.transform, false);
+        mapPlayer.transform.SetParent(dragableMap.transform, false);
         mapPlayer.transform.SetAsLastSibling();
     }
 }
---
+++ Assets/Scripts/MapDragHandeler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MapDragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Vector3 gap;//écart entre la position de la map et celle de la souris au moment du clic
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        gap = GetComponent<RectTransform>().anchoredPosition - (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        GetComponent<RectTransform>().anchoredPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + gap;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
